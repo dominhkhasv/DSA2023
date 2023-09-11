6336400f68e9e75c7df09b1828f1586ea7cf99fb
dominhkha <kha.minhdo199@gmail.com>
2023-09-11 15:53:30 +0700

    add SUm, UFClient, UFClient2

---
+++ src/main/week2/UFClient2.java
@@ -0,0 +1,27 @@
+package main.week2;
+
+import edu.princeton.cs.algs4.*;
+import org.w3c.dom.ls.LSOutput;
+
+public class UFClient2 {
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+        int pairCount = 0;
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            pairCount ++;
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+            }
+
+            if(uf.count() == 1){
+                System.out.println(pairCount);
+                return;
+            }
+        }
+        System.out.println("FAILED");
+    }
+
+}
